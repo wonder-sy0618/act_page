09b1fba166dea364f57fc3e4816ebf6b6d3d3916
shiying <wonder_sy0618@foxmail.com>
2018-05-26 21:12:20 +0800

    编译预览

--- music_love/src/pages/Proview.jsx
+++ music_love/src/pages/Proview.jsx
@@ -1,11 +1,18 @@
 
 import React, { Component } from 'react';
+import { Msg } from "react-weui";
+
 import Title from '../components/Title'
 
 export default (props) => (
   <div>
-    完成
-    <img src={props.proviewImg} style={{"width" : "100%"}} />
-
+    <Msg
+        type="success"
+        title="创建完成"
+        description="请长按下方图片，保存到手机"
+        ></Msg>
+    <div style={{textAlign : "center"}}>
+      <img src={props.proviewImg} style={{"width" : "90%"}} />
+    </div>
   </div>
 )
