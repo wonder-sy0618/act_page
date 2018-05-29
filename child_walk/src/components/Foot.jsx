978a1c2a613f9a915ab166409901ea596b60bafa
shiying <wonder_sy0618@foxmail.com>
2018-05-29 22:07:31 +0800

    修改资源

--- child_walk/src/components/Foot.jsx
+++ child_walk/src/components/Foot.jsx
@@ -6,8 +6,7 @@ import { Footer, FooterText, FooterLinks, FooterLink } from 'react-weui';
 export default (props) => (
   <Footer>
       <FooterText>
-        2018为爱行走大型公益徒步活动<br/>
-        主办方：西咸新区空港新城、陕西妇女儿童发展基金会、陕西广播电视台896汽车调频、当代陕西杂志社陕西网
+        该项目由陕西省少工委、陕西广播电视台896汽车调频、陕西省青少年发展基金会、西安邮电大学爱心青年帮、丝路青年网共同发起，旨在通过网络众筹，为陕西52个贫困县的孩子送一双合脚的运动鞋。
       </FooterText>
   </Footer>
 )
