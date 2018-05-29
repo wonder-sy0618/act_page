bfebd7292529b457b1ec95041587724217c3852d
shiying <wonder_sy0618@foxmail.com>
2018-05-29 21:46:42 +0800

    完善业务流程

--- child_walk/src/pages/Index.jsx
+++ child_walk/src/pages/Index.jsx
@@ -22,7 +22,19 @@ export default (props) => (
           <div className="btnContent">
            <img src={iconMake} />
            <br/>
-           <span>参与活动</span>
+           <span>制作专属海报</span>
+          </div>
+        </a>
+      </section>
+      <section className="SectionUpload" >
+        <a href="https://ssl.gongyi.qq.com/m/weixin/detail.htm?showwxpaytitle=1&et=fx&pid=41653"
+            className={props.uploading ? "btnUpload disabled" : "btnUpload"} style={{"margin-top" : -130}} >
+          <div className="shadow s1" ></div>
+          <div className="shadow s2" ></div>
+          <div className="btnContent">
+           <img src={require("../res/icon_love.svg")} />
+           <br/>
+           <span style={{"color" : "red"}}>点击捐款</span>
           </div>
         </a>
       </section>
