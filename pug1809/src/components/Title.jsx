05ffee804ebd4fb3d8ae37fd161763db997be5bf
shiying <wonder_sy0618@foxmail.com>
2018-09-19 15:36:29 +0800

    build

--- pug1809/src/components/Title.jsx
+++ pug1809/src/components/Title.jsx
@@ -1,8 +1,16 @@
 
 import React, { Component } from 'react';
 
-import imgTitle from "../res/title.png"
+import imgTitle from "../res/star1.jpg"
 
 export default (props) => (
-  <img src={imgTitle} className="Title" />
+  <img src={imgTitle} className="Title" style={{
+    "height": window.innerHeight,
+    "width": 1920 / 1280 * window.innerHeight,
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "maxWidth": "10000%",
+    "marginLeft": "-120px",
+  }} />
 )
