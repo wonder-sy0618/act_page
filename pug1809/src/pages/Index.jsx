85ce303e54563adb350fd9c45c080be7588f6973
shiying <wonder_sy0618@foxmail.com>
2018-09-20 10:29:09 +0800

    解决ios设备首页能进行滑动的问题

--- pug1809/src/pages/Index.jsx
+++ pug1809/src/pages/Index.jsx
@@ -1,32 +1,32 @@
+import React, { Component } from "react";
+import { Page, Article, Button } from "react-weui";
 
-import React, { Component } from 'react';
-import {Page, Article, Button } from 'react-weui';
+import Title from "../components/Title";
+import Foot from "../components/Foot";
 
-import Title from '../components/Title'
-import Foot from '../components/Foot'
+import iconMake from "../res/icon_make.svg";
+import "./Index.css";
 
-import iconMake from '../res/icon_make.svg'
-import "./Index.css"
-
-export default (props) => (
+export default props => (
   <Page className="Index" transition={false} ptr={false}>
-    <Article>
-      <section>
-        <Title></Title>
-      </section>
-      <section className="SectionUpload" >
-        <a href="#" className={props.uploading ? "btnUpload disabled" : "btnUpload"} >
-          <input type="file" accept="image/*" onChange={props.onSelectFile } />
-          <div className="shadow s1" ></div>
-          <div className="shadow s2" ></div>
-          <div className="btnContent">
-           <img src={iconMake} />
-           <br/>
-           <span>参与活动</span>
-          </div>
-        </a>
-      </section>
-    </Article>
-    <Foot></Foot>
+    <section>
+      <Title />
+    </section>
+    <section className="SectionUpload">
+      <a
+        href="#"
+        className={props.uploading ? "btnUpload disabled" : "btnUpload"}
+      >
+        <input type="file" accept="image/*" onChange={props.onSelectFile} />
+        <div className="shadow s1" />
+        <div className="shadow s2" />
+        <div className="btnContent">
+          <img src={iconMake} />
+          <br />
+          <span>参与活动</span>
+        </div>
+      </a>
+    </section>
+    <Foot />
   </Page>
-)
+);
