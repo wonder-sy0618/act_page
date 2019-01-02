65480c5032ac104aaabddb076c9c0c98c254815a
shiying <wonder_sy0618@foxmail.com>
2019-01-02 19:20:32 +0800

    首页背景显示优化

--- mom1901/src/pages/Index.jsx
+++ mom1901/src/pages/Index.jsx
@@ -7,53 +7,68 @@ import Foot from "../components/Foot";
 import iconMake from "../res/icon_make.svg";
 import "./Index.css";
 
-export default props => (
-  <div className="Index" style={{}}>
-    <div>
-      <Title {...props} />
-    </div>
-    <div
-      className="SectionUpload"
-      style={
-        props.prev && props.prev.headimg
-          ? {
-              marginTop: (1150 / 1300) * window.innerWidth + "px"
-            }
-          : {
-              marginTop: (1100 / 1300) * window.innerWidth + "px"
-            }
-      }
-    >
-      <a
-        href="#"
-        className={props.uploading ? "btnUpload disabled" : "btnUpload"}
-        style={{
-          width: (420 / 1300) * window.innerWidth,
-          height: (420 / 1300) * window.innerWidth
-        }}
-      >
-        <input type="file" accept="image/*" onChange={props.onSelectFile} />
-        <div
-          className="shadow s1"
-          style={{
-            width: (420 / 1300) * window.innerWidth,
-            height: (420 / 1300) * window.innerWidth
-          }}
-        />
-        <div
-          className="shadow s2"
-          style={{
-            width: (420 / 1300) * window.innerWidth,
-            height: (420 / 1300) * window.innerWidth
-          }}
-        />
-        <div className="btnContent">
-          <img src={iconMake} />
-          <br />
-          <span>参与活动</span>
+class Index extends Component {
+  constructor(props, context) {
+    super();
+  }
+  componentWillUnmount() {
+    // 为了避免首页大图加载时底色是白色，在加载前页面底色设置为红（index.html）
+    // 离开index时使设置失效
+    document.getElementsByTagName("html")[0].style.backgroundColor = "";
+  }
+  render() {
+    return (
+      <div className="Index" style={{}}>
+        <div>
+          <Title {...this.props} />
         </div>
-      </a>
-    </div>
-    <Foot />
-  </div>
-);
+        <div
+          className="SectionUpload"
+          style={
+            this.props.prev && this.props.prev.headimg
+              ? { marginTop: (1150 / 1300) * window.innerWidth + "px" }
+              : { marginTop: (1100 / 1300) * window.innerWidth + "px" }
+          }
+        >
+          <a
+            href="#"
+            className={
+              this.props.uploading ? "btnUpload disabled" : "btnUpload"
+            }
+            style={{
+              width: (420 / 1300) * window.innerWidth,
+              height: (420 / 1300) * window.innerWidth
+            }}
+          >
+            <input
+              type="file"
+              accept="image/*"
+              onChange={this.props.onSelectFile}
+            />
+            <div
+              className="shadow s1"
+              style={{
+                width: (420 / 1300) * window.innerWidth,
+                height: (420 / 1300) * window.innerWidth
+              }}
+            />
+            <div
+              className="shadow s2"
+              style={{
+                width: (420 / 1300) * window.innerWidth,
+                height: (420 / 1300) * window.innerWidth
+              }}
+            />
+            <div className="btnContent">
+              <img src={iconMake} />
+              <br />
+              <span>参与活动</span>
+            </div>
+          </a>
+        </div>
+        <Foot />
+      </div>
+    );
+  }
+}
+export default Index;
